1b4a141ec288a949fe2c7062bac763d4c89a14c7
Pakanon Pantisawat <pakanon.pk@gmail.com>
2021-06-11 10:51:00 +0700

    edited formattedMessUp to add quote enclosing

--- src/helpers/String.ts
+++ src/helpers/String.ts
@@ -1,18 +1,24 @@
 import { compose } from './Functions'
 
-/* eslint-disable no-irregular-whitespace */
-export const messup = (str: string): string =>
+/* eslint-disable no-irregular-whitespace  */
+/* eslint-disable import/prefer-default-export */
+const messup = (str: string): string =>
   str
     .split('')
     .map((v) => (Math.random() > 0.5 ? `${v}​` : v))
     .map((v) => (Math.random() > 0.7 ? `${v}​` : v))
     .join('​')
 
-export const toCapitalCase = (str: string): string =>
+const toCapitalCase = (str: string): string =>
   str
     .split('')
     .map((c: string, index: number) => (index === 0 ? c.toUpperCase() : c))
     .join('')
 
+const encloseWith = (encloser: string) => (str: string) =>
+  encloser + str + encloser
+
+const encloseWithQuote = (str: string): string => encloseWith('"')(str)
+
 export const formattedMessedUp = (str: string): string =>
-  compose(messup, toCapitalCase)(str)
+  compose(messup, encloseWithQuote, toCapitalCase)(str)
